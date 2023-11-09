063b8553a57a9d74265bc1d4dd1dca469f6cf0b1
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-11-09 10:47:50 -0300

    Add @astrojs/vercel dependency and update .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
+import react from '@astrojs/react';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic(),
   integrations: [react()]
 });
