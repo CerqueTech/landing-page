9256c0da4e89ac19c10019293770c215ad53ed61
Mateo Cerquetella <mateocerquetella@hotmail.com>
2023-11-10 11:05:08 -0300

    Update .gitignore and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
