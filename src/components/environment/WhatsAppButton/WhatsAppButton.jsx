c9441878ddc12b1255b76b193a9b84586c8903fb
Mateo Cerquetella <mateocerquetella@hotmail.com>
2024-01-02 12:36:12 -0300

    Add react-floating-whatsapp for enhanced user interaction
    Include a new feature to allow users to interact via WhatsApp directly
    on the website, providing quicker customer support and engagement.
    Corresponding back-to-top functionality has been removed as it is no
    longer necessary with the addition. No known issues. No issue
    references.

---
+++ src/components/environment/WhatsAppButton/WhatsAppButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+export default function WhatsAppButton() {
+  return (
+      <FloatingWhatsApp
+        phoneNumber="+541140541120"
+        accountName="Mateo Cerquetella"
+        chatMessage='¡Hola! ¿Cómo puedo ayudarte?'
+        statusMessage='Normalmente responde en 1 hora...'
+        placeholder= 'Ingresa tu mensaje...'
+        avatar="assets/images/whatsapp/icon.jpeg"
+        messageDelay={1.5}
+        chatboxHeight={400}
+        allowEsc
+        allowClickAway
+        notification
+        notificationDelay={20}
+        notificationSound
+      />
+  );
+}
