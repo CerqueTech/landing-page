6e9ef2b14e5c6d22852d8471f2c1b16fdfe984e9
kalilgo <gomezukalil@gmail.com>
2024-02-21 16:50:24 -0300

    responsive Logo footer

---
+++ src/components/environment/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import { $theme } from "../../../environment/theme";
+import { useStore } from "@nanostores/react";
+import "./Logo.css";
+const Logo = ({ srcwith, srcdark, alt }) => {
+  const theme = useStore($theme);
+  return <img className="logo" src={theme === "dark" ? srcwith : srcdark} alt={alt} />;
+};
+
+export default Logo;
